2b720a4797ee0678e6a6a1310e6b1da9e04bd37a
Leticia Ribeiro Miranda <letiiribeiro@gmail.com>
2017-04-13 03:00:18 +0000

    criando servico para puxar os dados do admin de secrets.yml e seed para admin

---
+++ app/services/create_admin_service.rb
@@ -0,0 +1,8 @@
+class CreateAdminService
+  def call
+    Admin.find_or_create_by!(email: Rails.application.secrets.admin_email) do |admin|
+        admin.password = Rails.application.secrets.admin_password
+        admin.password_confirmation = Rails.application.secrets.admin_password
+      end
+  end
+end
