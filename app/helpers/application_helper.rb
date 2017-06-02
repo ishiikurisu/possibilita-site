dbeaaa519c7b32ce9e8982e6985cdef996e49d65
Letícia Ribeiro <leticiaribeiro@aluno.unb.br>
2017-06-02 14:44:02 +0000

    traduzindo botões e labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def log_out_in_helper(current_admin)
     if(current_admin)
-      link_to("Admin Out", destroy_admin_session_path, :method => :delete, class: "btn btn-default")
+      link_to("Sair", destroy_admin_session_path, :method => :delete, class: "btn btn-default")
     else
       link_to("Eu sou administrador", new_admin_session_path, class: "btn btn-default")
     end
